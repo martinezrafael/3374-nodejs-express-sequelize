4e76f7116588ae334a82d3f2cb62ff1f962d6563
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-09-14 18:30:53 -0300

    adiciona mixins

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -11,12 +11,14 @@ router.get("/pessoas", (req, res) => pessoaController.pegaTodos(req, res));
 router.get("/pessoas/:id", (req, res) =>
   pessoaController.pegaUmPorId(req, res),
 );
+router.get("/pessoas/:estudanteId/matriculas", (req, res) =>
+  pessoaController.pegaMatriculas(req, res),
+);
 router.post("/pessoas", (req, res) => pessoaController.criaNovo(req, res));
 router.put("/pessoas/:id", (req, res) => pessoaController.atualiza(req, res));
-router.delete("/pessoas/:id", (req, res) => pessoaController.exclui(req, res));
-
 router.post("/pessoas/:estudanteId/matriculas", (req, res) =>
   matriculaController.criaNovo(req, res),
 );
+router.delete("/pessoas/:id", (req, res) => pessoaController.exclui(req, res));
 
 module.exports = router;
