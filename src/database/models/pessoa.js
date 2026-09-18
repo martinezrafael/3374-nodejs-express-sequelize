6072362ba085dd4210d0448045597dfab5cc4261
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-09-18 18:57:12 -0300

    valida cpf

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
-"use strict";
+const isCpfValido = require("../../utils/validaCpfHelper.js");
+
+("use strict");
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
@@ -15,9 +17,34 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoa.init(
     {
-      nome: DataTypes.STRING,
-      email: DataTypes.STRING,
-      cpf: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [3, 30],
+            msg: "O campo nome deve ter no mínimo 3 caracteres.",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Formato de e-mail inválido.",
+          },
+        },
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        validate: {
+          cpfEhValido: (cpf) => {
+            if (!isCpfValido(cpf)) {
+              throw new Error("Número de cpf inválido.");
+            }
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
       role: DataTypes.STRING,
     },
