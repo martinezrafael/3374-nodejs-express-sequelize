f303fd37f8cc7b1d1cf615d3d7655169a6f7500a
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-09-21 20:30:02 -0300

    cria a função helper para converter o id de string para number

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,3 +1,5 @@
+const converteIds = require("../utils/conversorDeStringHelper.js");
+
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
@@ -26,10 +28,9 @@ class Controller {
 
   async pegaUm(req, res) {
     const { ...params } = req.params;
-    console.log(...params);
-
+    const where = converteIds(params);
     try {
-      const umRegistro = await this.entidadeService.pegaUmRegistro(params);
+      const umRegistro = await this.entidadeService.pegaUmRegistro(where);
       return res.status(200).json(umRegistro);
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
