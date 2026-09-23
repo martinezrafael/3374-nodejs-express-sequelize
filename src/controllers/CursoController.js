81551f4eefd6ba27a5494c9a68e066e3dc08d631
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-09-23 09:52:54 -0300

    cria o metodo pegaCursos em `CursoController.js`

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const Controller = require("./Controller.js");
 const CursoService = require("../services/CursoServices.js");
 
@@ -6,6 +7,17 @@ const cursoServices = new CursoService();
 class CursoController extends Controller {
   constructor() {
     super(cursoServices);
+  }
+
+  async pegaCursos(req, res) {
+    const { data_inicial, data_final } = req.query;
+
+    //se existirem os param, criar uma prop {}
+    data_inicial || data_final ? (where.data_inicio = {}) : null;
+    // se existir data inicial, adiciona a prop gte com o valor
+    data_inicial ? (where.data_inicio[Op.gte] = data_inicial) : null;
+    // se existir data final, idem
+    data_final ? (where.data_inicio[Op.lte] = data_final) : null;
   }
 }
 
